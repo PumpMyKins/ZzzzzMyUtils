3fb94b75627426919a1daab66d0709dea2c92ead
XiliTra <nenel@DESKTOP-7TMF03B>
2019-05-24 21:03:32 +0200

    ajout commande delhome

--- ZzzzzMyUtils/src/java/fr/pumpmykins/zutils/commands/DelHomeCommand.java
+++ ZzzzzMyUtils/src/java/fr/pumpmykins/zutils/commands/DelHomeCommand.java
@@ -6,61 +6,104 @@ import fr.pumpmykins.zutils.utils.HomeData;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
 
 public class DelHomeCommand implements ICommand {
 
 	public DelHomeCommand(HomeData homedata) {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public int compareTo(ICommand o) {
-		// TODO Auto-generated method stub
+
+		
 		return 0;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		
+		
+		return "delhome";
 	}
 
 	@Override
 	public String getUsage(ICommandSender sender) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public List<String> getAliases() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
+		
+		if(sender instanceof Entityplayer) {
+			
+			EntityPlayer player = (EntityPlayer) sender;
+			
+			String homename = "";
+			
+			if(args.length < 0) {
+				
+				ITextComponent hm = new TextComponentString("Nom du home à suprimer");
+				hm.setStyle(PmkStyleTable.itemNumber());
+				sender.sendMessage(hm);
+				
+			} else {
+					
+					homename = args[0];
+					
+			}
+			
+			boolean toset = true;
+			
+			for(Home h : this.homedata.getHomeByUsername(player.getName())) {
+			
+				if(h.getHome_name() == homename) {
+					
+					toset = false;
+			
+			 
+				}
+				
+				if(toset) {
+					
+					h.setHome_Name = homename;
+					this.homedata.removeHome(h);
+				}
+			}
+		}
+		
 
 	}
 
 	@Override
 	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
 			BlockPos targetPos) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public boolean isUsernameIndex(String[] args, int index) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
