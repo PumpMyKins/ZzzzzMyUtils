4213535c20dc7271a1b334b30dd1eccf5993744c
Clem-Fern <clement.coussieu@gmail.com>
2019-05-25 14:39:26 +0200

    Re-Update directory

---
+++ src/java/fr/pumpmykins/zutils/MainPmkUtils.java
@@ -0,0 +1,149 @@
+package fr.pumpmykins.zutils;
+
+import org.apache.logging.log4j.Logger;
+
+import fr.pumpmykins.zutils.commands.DelHomeCommand;
+import fr.pumpmykins.zutils.commands.HomeCommand;
+import fr.pumpmykins.zutils.commands.SetHomeCommand;
+import fr.pumpmykins.zutils.commands.TpaCommand;
+import fr.pumpmykins.zutils.commands.TpaHereCommand;
+import fr.pumpmykins.zutils.utils.HomeData;
+import net.minecraft.world.World;
+import net.minecraft.world.storage.MapStorage;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(useMetadata=true, modid = "zpmkutils")
+public class MainPmkUtils {
+
+	@Instance("zpmkutils")
+	public static MainPmkUtils instance;
+	
+	public static Logger logger;
+
+	private static final String MODID = "zpmkutils";
+	
+	private static final String HOME_KEY = MODID+"_home";
+	
+	private static final String SPAWN_KEY = MODID+"_spawn";
+	
+	private HomeData homedata;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+		logger = event.getModLog();
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+		
+	}
+	
+	@EventHandler
+	public void onServerStarting(FMLServerStartingEvent event) {
+		
+		this.homedata = getHomeData(event.getServer().getWorld(0));
+		
+		event.registerServerCommand(new HomeCommand(this.homedata));
+		event.registerServerCommand(new SetSpawnCommand());
+		event.registerServerCommand(new DelHomeCommand(this.homedata));
+		event.registerServerCommand(new TpaCommand());
+		event.registerServerCommand(new TpaHereCommand());
+		event.registerServerCommand(new SetHomeCommand(this.homedata));
+	}
+	
+	@SubscribeEvent
+	public static void configChange(ConfigChangedEvent event) {
+		
+		if(event.getModID().equals(MODID)) {
+			ConfigManager.sync(MODID, Config.Type.INSTANCE);
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	@Config(modid = MODID)
+	public static class ModConfig{
+		
+		@Comment("Config for the ZzzzzMyUtils Mod")
+		
+		public static HomeSub homecat = new HomeSub();
+		private static class HomeSub {
+			public boolean active=true; 
+			public int quantity=2;
+		}
+		
+		public static SpawnSub spawncat = new SpawnSub();
+		private static class SpawnSub {
+			public boolean active=true; 
+		}
+		
+		public static TpaSub tpacat = new TpaSub();
+		private static class TpaSub {
+			public boolean active=true; 
+		}
+		
+	}
+
+	public static HomeData getHomeData(World w) {
+		
+		MapStorage storage = w.getMapStorage();
+		HomeData instance = (HomeData) storage.getOrLoadData(HomeData.class, HOME_KEY);
+		if(instance == null) {
+			
+			instance = new HomeData();
+			storage.setData(HOME_KEY, instance);
+		}
+		return instance;
+	}
+	
+	public static void setHomeData(World w) {
+	
+		MapStorage storage = w.getMapStorage();
+		HomeData instance = (HomeData) storage.getOrLoadData(HomeData.class, HOME_KEY);
+		if(instance.isDirty()) {
+			storage.setData(HOME_KEY, instance);
+		}
+	}
+	
+	public static MainPmkUtils getInstance() {
+		return instance;
+	}
+
+	public static void setInstance(MainPmkUtils instance) {
+		MainPmkUtils.instance = instance;
+	}
+
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	public static void setLogger(Logger logger) {
+		MainPmkUtils.logger = logger;
+	}
+
+	public static String getModid() {
+		return MODID;
+	}
+
+	public static String getHomeKey() {
+		return HOME_KEY;
+	}
+
+	public static String getSpawnKey() {
+		return SPAWN_KEY;
+	}
+	
+	
+}
---
+++ src/java/fr/pumpmykins/zutils/commands/SetHomeCommand.java
@@ -0,0 +1,111 @@
+package fr.pumpmykins.zutils.commands;
+
+import java.util.List;
+
+import fr.pumpmykins.zutils.utils.HomeData;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+
+public class SetHomeCommand implements ICommand {
+
+	private HomeData homedata;
+	
+	public SetHomeCommand(HomeData homedata) {
+
+		this.homedata = homedata;
+
+	}
+
+	@Override
+	public int compareTo(ICommand o) {
+
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+
+		return "sethome";
+	}
+
+	@Override
+	public String getUsage(ICommandSender sender) {
+
+		return null;
+	}
+
+	@Override
+	public List<String> getAliases() {
+
+		return null;
+	}
+
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+
+		if (sender instanceof Entityplayer) {
+
+			EntityPlayer player = (EntityPlayer) sender;
+
+			String homename = "";
+			if(args.length < 0) {
+
+				homename = "home";
+			} else {
+
+				homename = args[0];
+			}
+			
+			boolean toset = true;
+			
+			for(Home h : this.homedata.getHomeByUsername(player.getName())) {
+
+				if(h.getHome_name() == homename) {
+
+					// Send name already use
+					toset = false;
+					ITextComponent hm = new TextComponentString("Nom déjà utilisé");
+					hm.setStyle(PmkStyleTable.itemNumber());
+					sender.sendMessage(hm);
+				}
+			}
+			
+			if(toset) {
+				
+				Home h = new Home();
+				h.setHome_Name = homename;
+				// Set other properties 
+				
+				this.homedata.addHome(h);
+			}
+
+		}
+
+	}
+
+	@Override
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+
+		return false;
+	}
+
+	@Override
+	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
+			BlockPos targetPos) {
+
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] args, int index) {
+
+		return false;
+	}
+
+}
