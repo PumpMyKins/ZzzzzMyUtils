138724dac4a88c03933c5b95326cb3c6426aa49a
Sufike <l.christ54260@gmail.com>
2019-06-27 08:50:57 +0200

    add the module spawn

--- src/main/java/fr/pumpmykins/zutils/commands/SetSpawnCommand.java
+++ src/main/java/fr/pumpmykins/zutils/commands/SetSpawnCommand.java
@@ -1,61 +1,86 @@
 package fr.pumpmykins.zutils.commands;
 
+import java.util.Collections;
 import java.util.List;
 
+import fr.pumpmykins.zutils.MainPmkUtils.ModConfig;
+import fr.pumpmykins.zutils.utils.PmkStyleTable;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.server.permission.PermissionAPI;
 
 public class SetSpawnCommand implements ICommand {
 
 	@Override
 	public int compareTo(ICommand o) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "setspawn";
 	}
 
 	@Override
 	public String getUsage(ICommandSender sender) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "Syntax : /setspawn";
 	}
 
 	@Override
 	public List<String> getAliases() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
-
+		
+		if(sender instanceof EntityPlayer) {
+			
+			BlockPos pos = sender.getPosition();
+			
+			ModConfig.spawncat.setDim(sender.getEntityWorld().provider.getDimension());
+			ModConfig.spawncat.setX(pos.getX());;
+			ModConfig.spawncat.setY(pos.getY());;
+			ModConfig.spawncat.setZ(pos.getZ());;
+			
+			ITextComponent hm = new TextComponentString("Le nouveau ");
+			hm.setStyle(PmkStyleTable.italicBlue());
+			ITextComponent hm2 = new TextComponentString("spawn");
+			hm2.setStyle(PmkStyleTable.boldDarkRed());
+			ITextComponent hm3 = new TextComponentString(" à été établi avec succès !");
+			hm3.setStyle(PmkStyleTable.italicBlue());
+			hm.appendSibling(hm2);
+			hm.appendSibling(hm3);
+			sender.sendMessage(hm);
+		}
 	}
 
 	@Override
 	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return PermissionAPI.hasPermission((EntityPlayer) sender, "zutils.command.setspawn");
 	}
 
 	@Override
 	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
 			BlockPos targetPos) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return Collections.emptyList();
 	}
 
 	@Override
 	public boolean isUsernameIndex(String[] args, int index) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
--- src/main/java/fr/pumpmykins/zutils/commands/SpawnCommand.java
+++ src/main/java/fr/pumpmykins/zutils/commands/SpawnCommand.java
@@ -1,10 +1,13 @@
 package fr.pumpmykins.zutils.commands;
 
+import java.util.Collections;
 import java.util.List;
 
+import fr.pumpmykins.zutils.MainPmkUtils.ModConfig;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 
@@ -12,45 +15,54 @@ public class SpawnCommand implements ICommand {
 
 	@Override
 	public int compareTo(ICommand o) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "spawn";
 	}
 
 	@Override
 	public String getUsage(ICommandSender sender) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return "Syntax : /spawn";
 	}
 
 	@Override
 	public List<String> getAliases() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
-
+		
+		if(sender instanceof EntityPlayer) {
+			
+			EntityPlayer player = (EntityPlayer) sender;
+			
+			player.setPosition(ModConfig.spawncat.x, ModConfig.spawncat.y, ModConfig.spawncat.z);
+			if(player.dimension != ModConfig.spawncat.dim) {
+				
+				player.changeDimension(ModConfig.spawncat.dim);
+			}
+		}
 	}
 
 	@Override
 	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return true;
 	}
 
 	@Override
 	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
 			BlockPos targetPos) {
 		// TODO Auto-generated method stub
-		return null;
+		return Collections.emptyList();
 	}
 
 	@Override
